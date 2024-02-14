f2e4a8f6d3385b88c2c02dce0a9c3b5e8225019b
gilbmporto <gilbmp@gmail.com>
2024-02-14 20:25:09 -0300

    Modified description of the app (included span element)

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,12 @@ export default function Home() {
           <h1 className="capitalize text-5xl md:text-7xl font-bold">
             job <span className="text-primary">tracking</span> app
           </h1>
-          <p className="leading-loose max-w-md mt-4">
+          <p className="leading-loose max-w-[468px] mt-4">
             Join us and discover how our Job Tracking App can transform your job
             search into a more organized, effective, and fulfilling venture. Let
             us help you keep track of your applications, deadlines, and
             interviews, all in one place, so you can focus on what truly
-            matters: landing your dream job.
+            matters: <span className=" font-bold">landing your dream job.</span>
           </p>
           <Button asChild className="mt-4">
             <Link href="/add-job">Get Started</Link>
